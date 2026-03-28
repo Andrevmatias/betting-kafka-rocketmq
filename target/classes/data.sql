27d24ba82043be5e3e8165fb4c9bd08a07a73fff
André Victória Matias <andre.vmatias@gmail.com>
2026-03-28 00:56:49 -0300

    chore: drop from gitignore

--- target/classes/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO BETS (id, user_id, event_id, event_market_id, event_winner_id, amount,status) VALUES (1, 1, 1, 1, 1, 100, 'PENDING');
-INSERT INTO BETS (id, user_id, event_id, event_market_id, event_winner_id, amount,status) VALUES (2, 2, 2, 1, 1, 200, 'PENDING');
-INSERT INTO BETS (id, user_id, event_id, event_market_id, event_winner_id, amount,status) VALUES (3, 2, 2, 1, 2, 300, 'PENDING');
-INSERT INTO BETS (id, user_id, event_id, event_market_id, event_winner_id, amount,status) VALUES (4, 3, 2, 1, 2, 400, 'PENDING');
-INSERT INTO BETS (id, user_id, event_id, event_market_id, event_winner_id, amount,status) VALUES (5, 4, 3, 1, 1, 500, 'PENDING');
